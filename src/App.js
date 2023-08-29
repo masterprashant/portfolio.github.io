b35855ab2c7db0b85bba1e776221558496dc3444
masterprashant <prashantprajapati9310@gmail.com>
2023-08-29 17:26:03 +0530

    react portfolio

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
 import Home from "./Pages/Home/Homescreen";
-
+import HeroSection from "../HeroSection";
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
           <Navbar />
+        <HeroSection />
+
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="*" element={<div>404 Not Found</div>}></Route>
--- src/Pages/Home/Homescreen/index.js
+++ src/Pages/Home/Homescreen/index.js
@@ -1,7 +1,7 @@
 import AboutMe from "../AboutMe";
 import ContactMe from "../ContactMe";
 import Footer from "../Footer";
-import HeroSection from "../HeroSection";
+
 import MyPortfolio from "../MyPortfolio";
 import MySkills from "../MySkills";
 import Testimonial from "../Testimonials";
@@ -9,7 +9,6 @@ import Testimonial from "../Testimonials";
 export default function Home() {
   return (
     <>
-      <HeroSection />
       <AboutMe />
       <MySkills />
       {/* <MyPortfolio /> */}
